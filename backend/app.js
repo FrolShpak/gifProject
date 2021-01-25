e643e40fc6dfa4dd9de7145201c60630f36e357c
Frol Shpak <frol.shpak@gmail.com>
2021-01-25 18:12:59 -0500

    Add eslint
    add eslint
    add config for eslint
    add eslint-config-prettier so they are working together

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,13 @@
-import express from "express";
-import config from "./config/config";
+import express from 'express';
+import config from './config/config';
 
 const app = express();
 
-console.log(config);
+console.log('config:', config);
 const port = config.app.port;
-app.listen(port);
+
+app.get('/ping', (req, res) => res.send('pong'));
+
+app.listen(port, () => {
+  console.log('Running on port:', port);
+});
--- backend/config/config.js
+++ backend/config/config.js
@@ -1,26 +1,26 @@
-import dotenv from "dotenv";
-import convict from "convict";
+import dotenv from 'dotenv';
+import convict from 'convict';
 
 dotenv.config();
 
 const config = convict({
   env: {
-    format: ["prod", "dev", "test"],
-    default: "dev",
-    env: "NODE_ENV",
-    arg: "nodeEnv",
+    format: ['prod', 'dev', 'test'],
+    default: 'dev',
+    env: 'NODE_ENV',
+    arg: 'nodeEnv',
   },
   app: {
     port: {
-      format: "port",
+      format: 'port',
       default: 3000,
-      env: "PORT",
+      env: 'PORT',
     },
   },
 });
 
-const env = config.get("env");
+const env = config.get('env');
 config.loadFile(`./config/${env}.json`);
-config.validate({ allowed: "strict" });
+config.validate({ allowed: 'strict' });
 
 export default config.getProperties();
