ccd39bb5ac25b3f101ca269f622eb9ce2aa1a441
Frol Shpak <frol.shpak@gmail.com>
2021-01-30 15:21:29 -0500

    Truly successfully deploy

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,9 @@
+import 'regenerator-runtime/runtime.js';
 import express from 'express';
 import loaders from './loaders';
 import config from './config';
 
 async function startServer() {
-  console.log('config:', config);
-
   const app = express();
   await loaders({ expressApp: app });
 
